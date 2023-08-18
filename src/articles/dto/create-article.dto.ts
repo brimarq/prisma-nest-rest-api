c81cfc157b453e54302104bfe84a1d4824ef78d0
Brian Marquis <rbmarquis.dev@gmail.com>
2023-08-18 18:04:54 -0500

    add ValidationPipe and class validators

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,4 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 // DTO (Data Transfer Object) is an object that defines how the data will be sent over the network.
 
@@ -7,15 +15,26 @@ import { ApiProperty } from '@nestjs/swagger';
 
 // Define the shape of the request body
 export class CreateArticleDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
   @ApiProperty()
   title: string;
 
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @MaxLength(300)
   @ApiProperty({ required: false })
   description: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   body: string;
 
+  @IsBoolean()
+  @IsOptional()
   @ApiProperty({ required: false, default: false })
   published?: boolean = false;
 }
